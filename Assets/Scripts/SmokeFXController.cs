6e9baee3b226a1b213c5adc46e28fdaa2b3aa6a8
Yinon Ezra <yinon.ezra@gmail.com>
2025-01-05 09:56:43 +0200

    SmokeFix

--- Assets/Scripts/SmokeFXController.cs
+++ Assets/Scripts/SmokeFXController.cs
@@ -1,16 +1,29 @@
+using System;
 using UnityEngine;
-
+[ExecuteAlways]
 public class SmokeFXController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] Renderer[] rend;
+    [SerializeField] float _speed = 0.5f;
+    [SerializeField] AnimationCurve _curve;
+    private float _updatedMask;
+    [SerializeField] float _t;
+    private void Update()
     {
-        
+        AnimationLoop();
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    private void AnimationLoop()
     {
-        
+        if (_t < 1)
+        {
+            _t += Time.deltaTime * _speed;
+            _updatedMask = Mathf.Lerp(0,0.55f,_curve.Evaluate(_t));
+            for (int i = 0; i < rend.Length; i++)
+            {
+                rend[i].material.SetFloat("_Mask", _updatedMask);
+                rend[i].material.SetFloat("_Seed", i*1.0f);
+            }
+        }
     }
 }
